0ef70fdc5e5a82c58f7fda64925cb18afed875e2
Ndev <jncho1717@gmail.Com>
2024-08-18 22:29:01 +0200

    premier commit du site static

---
+++ js/index.js
@@ -0,0 +1,54 @@
+// lis.forEach(element => {
+//     console.log(element);
+    
+//     if(url.includes(home)){
+//         element.className = "active";
+//     }
+// });
+
+const goToDashbord = ()=>{
+    window.location.href = "home.html";
+}
+
+const goToEleve = ()=>{
+    window.location.href = "student.html";
+}
+
+const goToProfesseur = ()=>{
+    window.location.href = "teachers.html";
+}
+
+const goToUtilisateur = ()=>{
+    window.location.href = "users.html";
+}
+
+const goToRapport = ()=>{
+    window.location.href = "rapport.html";
+}
+
+const goToAddEleve = ()=>{
+    window.location.href = "addStudent.html";
+}
+
+const goToAddProfesseur = ()=>{
+    window.location.href = "addTeacher.html";
+}
+
+const goToAddUser = ()=>{
+    window.location.href = "addUser.html";
+}
+const goToLogin = ()=>{
+    window.location.href = "login.html";
+}
+
+const goToUpdateStudent = ()=>{
+    window.location.href = "updateStudent.html"
+}
+
+const goToUpdateProfesseur = ()=>{
+    window.location.href = "updateTeacher.html"
+}
+
+const goToUpdateUtilisateur = ()=>{
+    window.location.href = "updateUser.html"
+}
